88fe4f3170480b57075138e7d5115cf73374b164
chomado <chomado@gmail.com>
2017-04-27 20:12:15 +0900

    空っぽの GetAssemblyTimeStamp 作った

---
+++ AssemblyTimeStamp.PCL/TimeStampUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AssemblyTimeStamp
+{
+	public static class TimeStampUtil
+	{
+		public static DateTime GetAssemblyTimeStamp()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
