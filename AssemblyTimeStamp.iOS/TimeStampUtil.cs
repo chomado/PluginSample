9ae10ce011fff4d05aeb773204deb22bf8385290
chomado <chomado@gmail.com>
2017-04-27 20:31:43 +0900

    PCLから呼べない処理を、iOSプロジェクトに追加

---
+++ AssemblyTimeStamp.iOS/TimeStampUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AssemblyTimeStamp
+{
+	public static class TimeStampUtil
+	{
+		public static DateTime GetAssemblyTimeStamp()
+		{
+			return System.IO.File.GetLastWriteTime(
+				path: "AssemblyTimeStamp.dll" // 自分
+			);
+		}
+	}
+}
