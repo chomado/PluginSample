07e640a8d2f91c3a89973cdd3f3ff3741a55c586
chomado <chomado@gmail.com>
2017-04-27 21:02:13 +0900

    Android 側の実装も追加

--- AssemblyTimeStamp.Droid/TimeStampUtil.cs
+++ AssemblyTimeStamp.Droid/TimeStampUtil.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Reflection;
+
 namespace AssemblyTimeStamp
 {
 	public static class TimeStampUtil
 	{
 		public static DateTime GetAssemblyTimeStamp()
 		{
+			//var assemblyPath = Assembly.GetExecutingAssembly().Location;
+			//System.Diagnostics.Debug.WriteLine("assemblyPath: " + assemblyPath);
+			//// /storage/emulated/0/Android/data/com.chomado.pluginsample/files/.__override__/AssemblyTimeStamp.dll
+
+			//var assemblyDirectoryPath = System.IO.Path.GetDirectoryName(assemblyPath);
+			//System.Diagnostics.Debug.WriteLine("assemblyDirectoryPath: " + assemblyDirectoryPath);
+			//// /storage/emulated/0/Android/data/com.chomado.pluginsample/files/.__override__
+
+			//var assemblyTimeStampDllPath = System.IO.Path.Combine(
+			//	assemblyDirectoryPath,
+			//	"AssemblyTimeStamp.dll");
+			//System.Diagnostics.Debug.WriteLine("assemblyTimeStampDllPath: " + assemblyTimeStampDllPath);
+			//// /storage/emulated/0/Android/data/com.chomado.pluginsample/files/.__override__/AssemblyTimeStamp.dll
+
+			//var current = System.IO.Path.GetFullPath(".");
+			//System.Diagnostics.Debug.WriteLine("current: " + current);
+			//// current: /
+
+			var assemblyTimeStampDllPath = typeof(TimeStampUtil).Assembly.Location; 
+
 			return System.IO.File.GetLastWriteTime(
-				path: "AssemblyTimeStamp.dll" // 自分
+				path: assemblyTimeStampDllPath
 			);
 		}
 	}
--- AssemblyTimeStamp.iOS/TimeStampUtil.cs
+++ AssemblyTimeStamp.iOS/TimeStampUtil.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Reflection;
+
 namespace AssemblyTimeStamp
 {
 	public static class TimeStampUtil
 	{
 		public static DateTime GetAssemblyTimeStamp()
 		{
+			//var assemblyTimeStampDllPath = typeof(TimeStampUtil).Assembly.Location;
+			//System.Diagnostics.Debug.WriteLine(System.IO.Path.GetFullPath("."));
+			//return System.IO.File.GetLastWriteTime(
+			//	path: assemblyTimeStampDllPath
+			//);
 			return System.IO.File.GetLastWriteTime(
 				path: "AssemblyTimeStamp.dll" // 自分
 			);
